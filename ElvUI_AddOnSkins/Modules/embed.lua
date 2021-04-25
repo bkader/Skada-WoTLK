6d4308d1f70d35563b8c7a2f058c7857846e321b
Kader Bouyakoub <bkader@mail.com>
2021-04-25 23:57:21 +0100

    last fix

--- ElvUI_AddOnSkins/Modules/embed.lua
+++ ElvUI_AddOnSkins/Modules/embed.lua
@@ -373,7 +373,7 @@ if AS:IsAddonLODorEnabled("Skada") then
 
         if Skada.revisited then
             window.db.scale = 1
-            window.db.background.height = height + (window.db.enabletitle and E.Spacing or 0) - (E.Border + E.Spacing)
+            window.db.background.height = height + (window.db.enabletitle and window.db.title.height or 0) - (E.Border + E.Spacing)
 
             local offsety = window.db.reversegrowth and 0 or E.Border
             window.bargroup.ClearAllPoints = nil
