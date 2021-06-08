e186bdf118edd752c960bedc3d279479140b6871
Kader Bouyakoub <bkader@mail.com>
2021-06-08 11:03:18 +0100

    Update Spamage.lua

--- Skada/Modules/Spamage.lua
+++ Skada/Modules/Spamage.lua
@@ -157,7 +157,7 @@ Skada:AddLoadableModule("Spamage", "Suppresses chat messages from damage meters 
 	local nextlines = {
 		"^(%d+). (.*)$", --Recount, Details! and Skada
 		"^(%d+). (.*)$", --Skada
-		"^(.*) (.*)$", -- Additional Skada
+		"^(.*)  (.*)$", -- Additional Skada
 		"^.*%%%)$", --Skada player details
 		"^[+-]%d+.%d", -- Numeration deathlog details
 		"^(%d+). (.*):(.*)(%d+)(.*)(%d+)%%(.*)%((%d+)%)$" -- TinyDPS
