2fd6dc108d7a94df8c5b1630cccaf73ba5ecf2d0
Kader Bouyakoub <bkader@mail.com>
2021-06-18 10:53:52 +0100

    Update Locales.lua

--- SkadaImprovement/Locales.lua
+++ SkadaImprovement/Locales.lua
@@ -4,11 +4,11 @@ if not Skada then return end
 local L = Skada.L
 
 -- default to enUS/enGB
-L["Improvement"] = "Improvement"
-L["Improvement modes"] = "Improvement modes"
-L["Improvement comparison"] = "Improvement comparison"
-L["Do you want to reset your improvement data?"] = "Do you want to reset your improvement data?"
-L["%s's overall data"] = "%s's overall data"
+L["Improvement"] = true
+L["Improvement modes"] = true
+L["Improvement comparison"] = true
+L["Do you want to reset your improvement data?"] = true
+L["%s's overall data"] = true
 
 -- deDE
 if Skada.locale == "deDE" then
