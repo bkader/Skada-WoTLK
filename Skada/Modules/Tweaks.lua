b7361b815fe80672268b706c1a7e62f014a66fc8
Kader Bouyakoub <bkader@mail.com>
2021-06-24 20:22:03 +0100

    no more spamage module

--- Skada/Modules/Tweaks.lua
+++ Skada/Modules/Tweaks.lua
@@ -8,12 +8,8 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 	local UnitExists, UnitName, UnitClass = UnitExists, UnitName, UnitClass
 	local GetSpellLink, GetSpellInfo = Skada.GetSpellLink, Skada.GetSpellInfo
 
-	local BITMASK_GROUP = Skada.BITMASK_GROUP
-	local BITMASK_PETS = Skada.BITMASK_PETS
-	local BITMASK_OWNERS = Skada.BITMASK_OWNERS
-	local BITMASK_ENEMY = Skada.BITMASK_ENEMY
-
-	local pull_timer
+	local BITMASK_GROUP = bit.bor(COMBATLOG_OBJECT_AFFILIATION_MINE, COMBATLOG_OBJECT_AFFILIATION_PARTY, COMBATLOG_OBJECT_AFFILIATION_RAID)
+	local pull_timer, channelEvents
 
 	local ignoredspells = {
 		[1130] = true, -- Hunter's Mark (rank 1)
@@ -21,7 +17,7 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 		[14324] = true, -- Hunter's Mark (rank 3)
 		[14325] = true, -- Hunter's Mark (rank 4)
 		[53338] = true, -- Hunter's Mark (rank 5)
-		[60122] = true  -- Baby Spice
+		[60122] = true -- Baby Spice
 	}
 
 	-- thank you Details!
@@ -65,8 +61,7 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 
 		function mod:CombatLogEvent(_, _, timestamp, eventtype, srcGUID, srcName, srcFlags, dstGUID, dstName, dstFlags, ...)
 			-- pull timer
-			if Skada.db.profile.firsthit and (triggerevents[eventtype] or eventtype == "SPELL_CAST_SUCCESS") and not pull_timer then
-				if ignoredspells[select(1, ...)] then return end
+			if (triggerevents[eventtype] or eventtype == "SPELL_CAST_SUCCESS") and not pull_timer and not ignoredspells[select(1, ...)] then
 				if srcName and dstName and ((band(srcFlags, BITMASK_GROUP) ~= 0 and Skada:IsBoss(dstGUID)) or (band(dstFlags, BITMASK_GROUP) ~= 0 and Skada:IsBoss(srcGUID))) then
 					local puller
 
@@ -121,6 +116,150 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 
 	---------------------------------------------------------------------------
 
+	do
+		local find, gsub, split, tonumber = string.find, string.gsub, string.split, tonumber
+		local GetTime = GetTime
+		local ShowUIPanel = ShowUIPanel
+		local ItemRefTooltip = ItemRefTooltip
+
+		local firstlines = {
+			"^Details!: (.*)$", -- Details!
+			"^Skada report on (.*) for (.*), (.*) to (.*):$", -- Skada enUS
+			"^(.*) - (.*)의 Skada 보고, (.*) ~ (.*):$", -- Skada koKR
+			"^Skada报告(.*)的(.*), (.*)到(.*):$", -- Skada zhCN
+			"^(.*)的報告來自(.*)，從(.*)到(.*)：$", -- Skada zhTW
+			"^Skada: (.*) for (.*), (.*) - (.*):$", -- Better Skada support player details
+			"^Recount - (.*)$", -- Recount
+			"^Skada: (.*) for (.*):$", -- Skada enUS
+			"^Skada: (.*) für (.*):$", -- Skada deDE
+			"^Skada: (.*) pour (.*):$", -- Skada frFR
+			"^Skada: (.*) для (.*):$", -- Skada ruRU
+			"^Отчёт Skada: (.*), с (.*):$", -- Skada ruRU
+			"^Skada: (.*) por (.*):$", -- Skada esES/ptBR
+			"^(.*) 의 Skada 보고 (.*):$", -- Skada koKR
+			"^Skada报告(.*)的(.*):$", -- Skada zhCN
+			"^Skada:(.*)來自(.*):$", -- Skada zhTW
+			"^(.*) Done for (.*)$", -- TinyDPS enUS
+			"^(.*) für (.*)$", -- TinyDPS deDE
+			"데미지량 -(.*)$", -- TinyDPS koKR
+			"힐량 -(.*)$", -- TinyDPS koKR
+			"Урон:(.*)$", -- TinyDPS ruRU
+			"Исцеление:(.*)$", -- TinyDPS ruRU
+			"^# (.*) - (.*)$", -- Numeration
+			"alDamageMeter : (.*)$", -- alDamageMeter
+			"^Details! Report for (.*)$" -- Details!
+		}
+
+		local nextlines = {
+			"^(%d+)%. (.*)$", -- Recount, Details! and Skada
+			"^ (%d+). (.*)$", -- Skada (default)
+			"^(.*)%s%s%s(.*)$", -- Additional Skada details
+			"^.*%%%)$", --Skada player details
+			"^[+-]%d+.%d", -- Numeration deathlog details
+			"^(%d+). (.*):(.*)(%d+)(.*)(%d+)%%(.*)%((%d+)%)$" -- TinyDPS
+		}
+
+		channelEvents = {
+			"CHAT_MSG_CHANNEL",
+			"CHAT_MSG_GUILD",
+			"CHAT_MSG_OFFICER",
+			"CHAT_MSG_PARTY",
+			"CHAT_MSG_PARTY_LEADER",
+			"CHAT_MSG_RAID",
+			"CHAT_MSG_RAID_LEADER",
+			"CHAT_MSG_SAY",
+			"CHAT_MSG_WHISPER",
+			"CHAT_MSG_WHISPER_INFORM",
+			"CHAT_MSG_BN_WHISPER",
+			"CHAT_MSG_BN_WHISPER_INFORM",
+			"CHAT_MSG_YELL"
+		}
+
+		local meters = {}
+
+		function mod:FilterLine(event, source, msg, ...)
+			for _, line in ipairs(nextlines) do
+				if msg:match(line) then
+					local curtime = GetTime()
+					for _, meter in ipairs(meters) do
+						local elapsed = curtime - meter.time
+						if meter.src == source and meter.evt == event and elapsed < 1 then
+							local toInsert = true
+							for _, b in ipairs(meter.data) do
+								if b == msg then
+									toInsert = false
+								end
+							end
+							if toInsert then
+								tinsert(meter.data, msg)
+							end
+							return true, false, nil
+						end
+					end
+				end
+			end
+
+			for i, line in ipairs(firstlines) do
+				local newID = 0
+				if msg:match(line) then
+					local curtime = GetTime()
+					if find(msg, "|cff(.+)|r") then
+						msg = gsub(msg, "|cff%w%w%w%w%w%w", "")
+						msg = gsub(msg, "|r", "")
+					end
+					for id, meter in ipairs(meters) do
+						local elapsed = curtime - meter.time
+						if meter.src == source and meter.evt == event and elapsed < 1 then
+							newID = id
+							return true, true, format("|HSKSP:%1$d|h|cffffff00[%2$s]|r|h", newID or 0, msg or "nil")
+						end
+					end
+					tinsert(meters, {src = source, evt = event, time = curtime, data = {}, title = msg})
+					for id, meter in ipairs(meters) do
+						if meter.src == source and meter.evt == event and meter.time == curtime then
+							newID = id
+						end
+					end
+					return true, true, format("|HSKSP:%1$d|h|cffffff00[%2$s]|r|h", newID or 0, msg or "nil")
+				end
+			end
+			return false, false, nil
+		end
+
+		function mod:ParseChatEvent(event, msg, sender, ...)
+			local ismeter, isfirstline, message = mod:FilterLine(event, sender, msg)
+			if ismeter then
+				if isfirstline then
+					return false, message, sender, ...
+				else
+					return true
+				end
+			end
+		end
+
+		function mod:ParseLink(link, text, button, chatframe)
+			local linktype, id = split(":", link)
+			if linktype == "SKSP" then
+				local meterid = tonumber(id)
+				ShowUIPanel(ItemRefTooltip)
+				if not ItemRefTooltip:IsShown() then
+					ItemRefTooltip:SetOwner(UIParent, "ANCHOR_PRESERVE")
+				end
+				ItemRefTooltip:ClearLines()
+				ItemRefTooltip:AddLine(meters[meterid].title)
+				ItemRefTooltip:AddLine(format(L["Reported by: %s"], meters[meterid].src))
+				for _, line in ipairs(meters[meterid].data) do
+					ItemRefTooltip:AddLine(line, 1, 1, 1)
+				end
+				ItemRefTooltip:Show()
+			else
+				return mod.hooks.SetItemRef(link, text, button, chatframe)
+			end
+		end
+	end
+
+	---------------------------------------------------------------------------
+
 	function mod:OnInitialize()
 		-- first hit.
 		if Skada.db.profile.firsthit == nil then
@@ -134,6 +273,11 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 			Skada.db.profile.smartwait = 5
 		end
 
+		-- old spamage module
+		if type(Skada.db.profile.spamage) == "table" or Skada.db.profile.spamage == nil then
+			Skada.db.profile.spamage = true
+		end
+
 		-- options.
 		Skada.options.args.Tweaks = {
 			type = "group",
@@ -143,7 +287,7 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 			end,
 			set = function(i, val)
 				Skada.db.profile[i[#i]] = val
-				Skada:ApplySettings()
+				mod:ApplySettings()
 			end,
 			order = 997,
 			args = {
@@ -158,6 +302,12 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 					name = L["Module Icons"],
 					desc = L["Enable this if you want to show module icons on windows and menus."],
 					order = 2
+				},
+				spamage = {
+					type = "toggle",
+					name = L["Filter DPS meters Spam"],
+					desc = L["Suppresses chat messages from damage meters and provides single chat-link damage statistics in a popup."],
+					order = 3
 				},
 				smartsep = {
 					type = "description",
@@ -186,8 +336,13 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 					type = "range",
 					name = L["Duration"],
 					desc = L["For how long Skada should wait before stopping the segment."],
-					disabled = function() return not Skada.db.profile.smartstop end,
-					min = 0, max = 10, step = 0.01, bigStep = 0.1,
+					disabled = function()
+						return not Skada.db.profile.smartstop
+					end,
+					min = 0,
+					max = 10,
+					step = 0.01,
+					bigStep = 0.1,
 					order = 95.4
 				}
 			}
@@ -195,11 +350,7 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 	end
 
 	function mod:BossDefeated(event, set)
-		if not (event == "COMBAT_BOSS_DEFEATED" and set) then
-			return
-		end
-
-		if Skada.db.profile.smartstop then
+		if event == "COMBAT_BOSS_DEFEATED" and set and not set.stopped then
 			Skada.After(Skada.db.profile.smartwait or 5, function()
 				if not set.endtime then
 					Skada:Print(L["Smart Stop"])
@@ -210,13 +361,13 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 	end
 
 	function mod:ModuleIconsBar(_, bar, mode)
-		if Skada.db.profile.moduleicons and bar and mode and mode.metadata then
+		if bar and mode and mode.metadata then
 			bar.icon = mode.metadata.icon
 		end
 	end
 
 	function mod:ModuleIconsMenu(_, info, mode)
-		if Skada.db.profile.moduleicons and info and mode and mode.metadata then
+		if info and mode and mode.metadata then
 			info.icon = mode.metadata and mode.metadata.icon
 			info.tCoordLeft = 0.065
 			info.tCoordRight = 0.935
@@ -226,12 +377,59 @@ Skada:AddLoadableModule("Tweaks", function(Skada, L)
 		end
 	end
 
+	function mod:ApplySettings()
+		-- first hit
+		if Skada.db.profile.firsthit then
+			if not self:IsHooked(Skada, "CombatLogEvent") then
+				self:SecureHook(Skada, "CombatLogEvent")
+			end
+			if not self:IsHooked(Skada, "EndSegment") then
+				self:SecureHook(Skada, "EndSegment")
+			end
+		else
+			if self:IsHooked(Skada, "CombatLogEvent") then
+				self:Unhook(Skada, "CombatLogEvent")
+			end
+			if self:IsHooked(Skada, "EndSegment") then
+				self:EndSegment() -- remove timer
+				self:Unhook(Skada, "EndSegment")
+			end
+		end
+
+		-- smart stop
+		if Skada.db.profile.smartstop then
+			Skada.RegisterCallback(self, "COMBAT_BOSS_DEFEATED", "BossDefeated")
+		else
+			Skada.UnregisterCallback(self, "COMBAT_BOSS_DEFEATED")
+		end
+
+		-- module icons
+		if Skada.db.profile.moduleicons then
+			Skada.RegisterCallback(self, "SKADA_MODE_BAR", "ModuleIconsBar")
+			Skada.RegisterCallback(self, "SKADA_MODE_MENU", "ModuleIconsMenu")
+		else
+			Skada.UnregisterCallback(self, "SKADA_MODE_BAR", "ModuleIconsBar")
+			Skada.UnregisterCallback(self, "SKADA_MODE_MENU", "ModuleIconsMenu")
+		end
+
+		-- filter dps meters
+		if Skada.db.profile.spamage then
+			if not self:IsHooked("SetItemRef") then
+				self:RawHook("SetItemRef", "ParseLink", true)
+			end
+			for _, e in ipairs(channelEvents) do
+				ChatFrame_AddMessageEventFilter(e, self.ParseChatEvent)
+			end
+		elseif self:IsHooked("SetItemRef") then
+			self:Unhook("SetItemRef")
+			for _, e in ipairs(channelEvents) do
+				ChatFrame_RemoveMessageEventFilter(e, self.ParseChatEvent)
+			end
+		end
+	end
+
 	function mod:OnEnable()
-		self:SecureHook(Skada, "CombatLogEvent")
-		self:SecureHook(Skada, "EndSegment")
-		Skada.RegisterCallback(self, "COMBAT_BOSS_DEFEATED", "BossDefeated")
-		Skada.RegisterCallback(self, "SKADA_MODE_BAR", "ModuleIconsBar")
-		Skada.RegisterCallback(self, "SKADA_MODE_MENU", "ModuleIconsMenu")
+		self:ApplySettings()
 	end
 
 	function mod:OnDisable()
