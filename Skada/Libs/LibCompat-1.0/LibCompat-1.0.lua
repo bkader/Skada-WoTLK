5f75bd0cd5801bfd6b29574d023889aab96f5b01
Kader Bouyakoub <bkader@mail.com>
2022-03-29 08:58:48 +0100

    Update LibCompat-1.0.lua

--- Skada/Libs/LibCompat-1.0/LibCompat-1.0.lua
+++ Skada/Libs/LibCompat-1.0/LibCompat-1.0.lua
@@ -86,22 +86,18 @@ do
 		end
 	end
 
-	local function tIndexOf(tbl, item)
-		for i, v in ipairs(tbl) do
-			if item == v then
-				return i
-			end
-		end
-	end
-
 	-- replace the global function
 	_G.tContains = function(tbl, item)
-		return (tIndexOf(tbl, item) ~= nil)
+		for _, v in pairs(tbl) do
+			if item == v then
+				return true
+			end
+		end
+		return false
 	end
 
 	lib.tLength = tLength
 	lib.tCopy = tCopy
-	lib.tIndexOf = tIndexOf
 end
 
 -------------------------------------------------------------------------------
@@ -746,7 +742,6 @@ local mixins = {
 	-- table util
 	"tLength",
 	"tCopy",
-	"tIndexOf",
 	"Table",
 	"TablePool",
 	-- roster util
