79c8cbe69f8b267cdd358f0ec02e537c1668e37f
Kader Bouyakoub <bkader@mail.com>
2023-10-18 09:48:30 +0100

    Update LibTranslit-1.0.lua

--- Skada/Libs/LibTranslit-1.0/LibTranslit-1.0.lua
+++ Skada/Libs/LibTranslit-1.0/LibTranslit-1.0.lua
@@ -1,16 +1,25 @@
+--[[
+Copyright (C) 2019-2022 Vardex
+This file is part of LibTranslit.
+LibTranslit is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+LibTranslit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License along with LibTranslit. If not, see <https://www.gnu.org/licenses/>. 
+--]]
+local MAJOR, MINOR = "LibTranslit-1.0", 4
 if not LibStub then
-	error(MAJOR .. " requires LibStub.")
+	error(("%s requires LibStub."):format(MAJOR))
 end
 
-local MAJOR, MINOR = "LibTranslit-1.0", 3
 local lib = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
 
 local strlen = strlen or string.len
+local strsub = strsub or string.sub
 local strbyte = strbyte or string.byte
 local strchar = strchar or string.char
+local format = format or string.format
 
-local CyrToLat = {
+local cyrToLat = {
 	["А"] = "A",
 	["а"] = "a",
 	["Б"] = "B",
@@ -80,31 +89,36 @@ local CyrToLat = {
 }
 
 function lib:Transliterate(str, mark)
-	if not str then
-		return ""
-	end
+	if not str then return "" end
 
 	mark = mark or ""
-	local tstr, marked, i = "", false, 1
+	local tstr = ""
+	local tword = ""
+	local mark_word = false
+	local i = 1
+
 	while i <= strlen(str) do
-		local c = str:sub(i, i)
+		local c = strsub(str, i, i)
 		local b = strbyte(c)
+
 		if b == 208 or b == 209 then
-			if marked == false then
-				tstr = tstr .. mark
-				marked = true
-			end
-			c = str:sub(i + 1, i + 1)
-			tstr = tstr .. (CyrToLat[strchar(b, strbyte(c))] or strchar(b, strbyte(c)))
+			mark_word = true
+			c = strsub(str, i + 1, i + 1)
+			tword = format("%s%s", tword, (cyrToLat[strchar(b, strbyte(c))] or strchar(b, strbyte(c))))
+
 			i = i + 2
 		else
+			tword = format("%s%s", tword, c)
+
 			if c == " " or c == "-" then
-				marked = false
+				tstr = format("%s%s", tstr, (mark_word and format("%s%s", mark, tword) or tword))
+				tword = ""
+				mark_word = false
 			end
-			tstr = tstr .. c
+
 			i = i + 1
 		end
 	end
 
-	return tstr
+	return format("%s%s", tstr, (mark_word and format("%s%s", mark, tword) or tword))
 end
